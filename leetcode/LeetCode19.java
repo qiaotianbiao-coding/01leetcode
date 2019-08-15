5b8d0220afbe2457b6181b4fc164feae6b470d8b
qiaotianbiao-coding <qiaotianbiao@outlook.com>
2019-08-15 22:38:33 +0800

    动态规划

--- leetcode/LeetCode19.java
+++ leetcode/LeetCode19.java
@@ -2,23 +2,19 @@ public class Main{c
     public ListNode removeNthFromEnd(ListNode head, int n) {
     ListNode dummy = new ListNode(0);
     dummy.next = head;
-     int length = 1;
-     int loca = 0;
-     ListNode node = dummy;
-     ListNode node2 = head;
-     while(node2.next!=null)
-     {
-        length++;
-        node2 = node2.next;
-     }
-     loca = length - n;
-     
-     while(loca>0)
-     {
-      loca--;
-      node = node.next;
-     }
-     node.next = node.next.next;
+    ListNode first = dummy;
+    ListNode second = dummy;
+    int m  = n;
+    if(m>0)
+    {
+        first = first.next;
+    }
+    while(first.next!=null)
+    {
+        first = first.next;
+        second = second.next;
+    }
+    second.next = second.next.next;
      return dummy.next;
     }
 
